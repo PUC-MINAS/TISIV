e68ff59ad1959e7f36ca2d0ed902308d33f63ea3
Lucas Rotsen <lucasrotsen@gmail.com>
2019-03-24 22:31:01 -0300

    Implementado formulário de cadastro

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,5 @@ Auth::routes();
 Route::get('/home', 'HomeController@index')->name('home');
 
 Route::get('/', 'HomeController@index')->name('home');
+
+Route::resource('cadastro', 'UsuarioController');
